de3e80fdfeeed1e792ba82a90102e83a4f7788f8
rty33 <rty3394@naver.com>
2023-04-28 19:26:30 +0900

    mola

--- PrimalEditor/GameDev/VisualStudio.cs
+++ PrimalEditor/GameDev/VisualStudio.cs
@@ -1,5 +1,4 @@
-﻿using EnvDTE;
-using EnvDTE80;
+﻿using PrimalEditor.GameProject;
 using PrimalEditor.Utilities;
 using System;
 using System.Collections.Generic;
@@ -12,15 +11,23 @@ using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Markup;
+using System.Windows.Media.Animation;
 
 namespace PrimalEditor.GameDev
 {
     static class VisualStudio
     {
+        public static bool BuildSucceeded { get; private set; } = true;
+        public static bool BuildDone { get; private set; } = true;
+
         private static EnvDTE80.DTE2 _vsInstance = null;
         private static readonly string _progID = "VisualStudio.DTE.17.0";
+
+        // interop constants for open visual studio Project
         [MarshalAs(UnmanagedType.LPStr)]
         public const string vsViewKindTextView = "{7651A703-06E5-11D1-8EBD-00A0C90F26EA}";
+
+
 
         [DllImport("ole32.dll")]
         private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
@@ -48,18 +55,18 @@ namespace PrimalEditor.GameDev
                     if (hResult < 0 || bindCtx == null) throw new COMException($"CreatedBindCtx() returned HREUSLT: {hResult:X8}");
 
                     IMoniker[] currentMoniker = new IMoniker[1];
-                    while(monikerTable.Next(1, currentMoniker, IntPtr.Zero) == 0)
+                    while (monikerTable.Next(1, currentMoniker, IntPtr.Zero) == 0)
                     {
                         string name = string.Empty;
                         currentMoniker[0]?.GetDisplayName(bindCtx, null, out name);
-                        if(name.Contains(_progID))
+                        if (name.Contains(_progID))
                         {
                             hResult = rot.GetObject(currentMoniker[0], out object obj);
                             if (hResult < 0 || obj == null) throw new COMException($"Running object table's GetObject() returned HRESULT: {hResult:X8}");
 
                             EnvDTE80.DTE2 dte = obj as EnvDTE80.DTE2;
                             var solutionName = dte.Solution.FullName;
-                            if(solutionName == solutionPath)
+                            if (solutionName == solutionPath)
                             {
                                 _vsInstance = dte;
                                 break;
@@ -89,7 +96,7 @@ namespace PrimalEditor.GameDev
         }
         public static void CloseVisualStudio()
         {
-            if(_vsInstance?.Solution.IsOpen == true)
+            if (_vsInstance?.Solution.IsOpen == true)
             {
                 _vsInstance.ExecuteCommand("File.SaveAll");
                 _vsInstance.Solution.Close(true);
@@ -103,16 +110,16 @@ namespace PrimalEditor.GameDev
             OpenVisualStudio(solution);
             try
             {
-                if(_vsInstance != null)
+                if (_vsInstance != null)
                 {
                     if (!_vsInstance.Solution.IsOpen) _vsInstance.Solution.Open(solution);
                     else _vsInstance.ExecuteCommand("File.SaveAll");
 
-                    foreach(EnvDTE.Project project in _vsInstance.Solution.Projects)
+                    foreach (EnvDTE.Project project in _vsInstance.Solution.Projects)
                     {
                         if (project.UniqueName.Contains(projectName))
                         {
-                            foreach(var file in files)
+                            foreach (var file in files)
                             {
                                 project.ProjectItems.AddFromFile(file);
                             }
@@ -120,9 +127,9 @@ namespace PrimalEditor.GameDev
                     }
 
                     var cpp = files.FirstOrDefault(x => Path.GetExtension(x) == ".cpp");
-                    if(!string.IsNullOrEmpty(cpp))
+                    if (!string.IsNullOrEmpty(cpp))
                     {
-                        _vsInstance.ItemOperations.OpenFile(cpp, vsViewKindTextView).Visible = true;    
+                        _vsInstance.ItemOperations.OpenFile(cpp, vsViewKindTextView).Visible = true;
                     }
                     _vsInstance.MainWindow.Activate();
                     _vsInstance.MainWindow.Visible = true;
@@ -135,6 +142,75 @@ namespace PrimalEditor.GameDev
                 return false;
             }
             return true;
-        }   
+        }
+
+        private static void OnBuildSolutionBegin(string project, string projectConfig, string platform, string solutionConfig)
+        {
+            Logger.Log(MessageType.Info, $"Building {project}, {projectConfig}, {platform}, {solutionConfig}");
+        }
+
+        private static void OnBuildSolutionDone(string project, string projectConfig, string platform, string solutionConfig, bool success)
+        {
+            if (BuildDone) return;
+
+            if (success) Logger.Log(MessageType.Info, $"Building {projectConfig} configuration succeeded");
+            else Logger.Log(MessageType.Error, $"Building {projectConfig} configuration failed");
+
+            BuildDone = true;
+            BuildSucceeded = success;
+        }
+
+        public static bool IsDebugging()
+        {
+            bool result = false;
+
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    result = _vsInstance != null &&
+                        (_vsInstance.Debugger.CurrentProgram != null || _vsInstance.Debugger.CurrentMode == EnvDTE.dbgDebugMode.dbgRunMode);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    if (!result) System.Threading.Thread.Sleep(1000);
+                }
+            }
+            return result;
+        }
+
+        public static void BuildSolution(Project project, string configName, bool showWindow = true)
+        {
+            if (IsDebugging())
+            {
+                Logger.Log(MessageType.Error, "Visual Studio is currently running process.");
+                return;
+            }
+
+            OpenVisualStudio(project.Solution);
+            BuildDone = BuildSucceeded = false;
+
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    if (!_vsInstance.Solution.IsOpen) _vsInstance.Solution.Open(project.Solution);
+                    _vsInstance.MainWindow.Visible = showWindow;
+
+                    _vsInstance.Events.BuildEvents.OnBuildProjConfigBegin += OnBuildSolutionBegin;
+                    _vsInstance.Events.BuildEvents.OnBuildProjConfigDone += OnBuildSolutionDone;
+
+                    _vsInstance.Solution.SolutionBuild.SolutionConfigurations.Item(configName).Activate();
+                    _vsInstance.ExecuteCommand("Build.BuildSolution");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    Debug.WriteLine($"Attempt {i}: failed to build {project.Name}");
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
     }
 }
