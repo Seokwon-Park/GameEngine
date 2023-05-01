1c0c691af5e7fce95f8adc6818d70dbe03b60c95
rty33 <rty3394@naver.com>
2023-05-01 13:41:12 +0900

    문제생기면 망함

--- EngineDLL/EngineAPI.cpp
+++ EngineDLL/EngineAPI.cpp
@@ -1,65 +1,36 @@
-#ifndef EDITOR_INTERFACE
-#define EDITOR_INTERFACE extern "C" __declspec(dllexport)
+#include "Common.h"
+#include "CommonHeaders.h"
+
+#ifndef WIN32_MEAN_AND_LEAN
+#define WIN32_MEAN_AND_LEAN
 #endif
 
-#include "CommonHeaders.h"
-#include "Id.h"
-#include "..\Engine\Components\Entity.h"
-#include "..\Engine\Components\Transform.h"
+#include <Windows.h>
 
 using namespace primal;
 
 namespace {
+	HMODULE game_code_dll{ nullptr };
 
-	struct transform_component
-	{
-		f32 position[3];
-		f32 rotation[3];
-		f32 scale[3];
+}//anonymouse namespace
 
-		transform::init_info to_init_info()
-		{
-			using namespace DirectX;
-			transform::init_info info{};
-			memcpy(&info.position[0], &position[0], sizeof(f32) * _countof(position));
-			memcpy(&info.scale[0], &scale[0], sizeof(f32) * _countof(scale));
-			XMFLOAT3A rot{ &rotation[0] };
-			XMVECTOR quat{ XMQuaternionRotationRollPitchYawFromVector(XMLoadFloat3A(&rot)) };
-			XMFLOAT4A rot_quat{};
-			XMStoreFloat4A(&rot_quat, quat);
-			memcpy(&info.rotation[0], &rot_quat.x, sizeof(f32) * _countof(info.rotation));
-			return info;
-		}
-		// XMQuaternionRotationRollPitchYawFromVector()
-		// XMQuaternionRotationRollPitchYawToVector()
-	};
-
-	struct game_entity_descriptor
-	{
-		transform_component transform;
-	};
-	
-	game_entity::entity entity_from_id(id::id_type id)
-	{
-		return game_entity::entity{ game_entity::entity_id{id} };
-	}
-
-} // anonymous namespace
-EDITOR_INTERFACE id::id_type CreateGameEntity(game_entity_descriptor* e)
+EDITOR_INTERFACE u32
+LoadGameCodeDll(const char* dll_path)
 {
-	assert(e);
-	game_entity_descriptor& desc{ *e };
-	transform::init_info transform_info{ desc.transform.to_init_info() };
-	game_entity::entity_info entity_info
-	{
-		&transform_info,
-	};
+	if (game_code_dll) return 0;
+	game_code_dll = LoadLibraryA(dll_path);
+	assert(game_code_dll);
 
-	return game_entity::create(entity_info).get_id();
+	return game_code_dll ? TRUE : FALSE;
 }
 
-EDITOR_INTERFACE void RemoveGameEntity(id::id_type id)
+EDITOR_INTERFACE u32
+UnloadGameCodeDll()
 {
-	assert(id::is_valid(id));
-	game_entity::remove(entity_from_id(id).get_id());
+	if (!game_code_dll) return FALSE;
+	assert(game_code_dll);
+	int result{ FreeLibrary(game_code_dll) };
+	assert(result);
+	game_code_dll = nullptr;
+	return TRUE;
 }
--- PrimalEditor/GameProject/Project.cs
+++ PrimalEditor/GameProject/Project.cs
@@ -1,4 +1,5 @@
-﻿using PrimalEditor.GameDev;
+﻿using PrimalEditor.DllWrappers;
+using PrimalEditor.GameDev;
 using PrimalEditor.Utilities;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 using System.Runtime.Serialization;
 using System.Text;
@@ -15,123 +17,198 @@ using System.Windows.Input;
 
 namespace PrimalEditor.GameProject
 {
-	[DataContract(Name = "Game")]
-	class Project : ViewModelBase
-	{
-		public static string Extension { get; } = ".primal";
-		[DataMember]
-		public string Name { get; private set; } = "New Project";
-		[DataMember]
-		public string Path { get; private set; }
+    enum BuildConfiguration
+    {
+        Debug,
+        DebugEditor,
+        Release,
+        ReleaseEditor,
+    }
 
-		public string FullPath => $@"{Path}{Name}{Extension}";
-		public string Solution => $@"{Path}{Name}.sln"; 
+    [DataContract(Name = "Game")]
+    class Project : ViewModelBase
+    {
+        public static string Extension { get; } = ".primal";
+        [DataMember]
+        public string Name { get; private set; } = "New Project";
+        [DataMember]
+        public string Path { get; private set; }
+        public string FullPath => $@"{Path}{Name}{Extension}";
+        public string Solution => $@"{Path}{Name}.sln";
+
+        private static readonly string[] _buildConfigurationNames = new string[] { "Debug", "DebugEditor", "Release", "ReleaseEditor" };
+        private int _buildConfig;
+        [DataMember]
+        public int BuildConfig
+        {
+            get => _buildConfig;
+            set
+            {
+                if (_buildConfig != value)
+                {
+                    _buildConfig = value;
+                    OnPropertyChanged(nameof(BuildConfig));
+                }
+            }
+        }
+
+        public BuildConfiguration StandAloneBuildConfig => BuildConfig == 0 ? BuildConfiguration.Debug : BuildConfiguration.Release;
+        public BuildConfiguration DllBuildConfig => BuildConfig == 0 ? BuildConfiguration.DebugEditor : BuildConfiguration.ReleaseEditor;
 
         [DataMember(Name = "Scenes")]
-		private ObservableCollection<Scene> _scenes = new ObservableCollection<Scene>();
-		public ReadOnlyObservableCollection<Scene> Scenes
-		{ get; private set; }
+        private ObservableCollection<Scene> _scenes = new ObservableCollection<Scene>();
+        public ReadOnlyObservableCollection<Scene> Scenes
+        { get; private set; }
 
-		private Scene _activeScene;
+        private Scene _activeScene;
 
-		public Scene ActiveScene
-		{
-			get { return _activeScene;}
-			set 
-			{ 
-				if(_activeScene!= value)
-				{
-					_activeScene = value;
-					OnPropertyChanged(nameof(ActiveScene));
-				}
-			}
-		}
+        public Scene ActiveScene
+        {
+            get { return _activeScene; }
+            set
+            {
+                if (_activeScene != value)
+                {
+                    _activeScene = value;
+                    OnPropertyChanged(nameof(ActiveScene));
+                }
+            }
+        }
 
-		public static Project Current => Application.Current.MainWindow.DataContext as Project;
+        public static Project Current => Application.Current.MainWindow.DataContext as Project;
 
-		public static UndoRedo UndoRedo { get; } = new UndoRedo();
-
+        public static UndoRedo UndoRedo { get; } = new UndoRedo();
 
         public ICommand UndoCommand { get; private set; }
         public ICommand RedoCommand { get; private set; }
 
         public ICommand AddSceneCommand { get; private set; }
-		public ICommand RemoveSceneCommand { get; private set; }		       
+        public ICommand RemoveSceneCommand { get; private set; }
         public ICommand SaveCommand { get; private set; }
-        
-        private void AddScene(string sceneName) 
-		{
-			Debug.Assert(!string.IsNullOrEmpty(sceneName.Trim()));
-			_scenes.Add(new Scene(this, sceneName));
-		}
+        public ICommand BuildCommand { get; private set; }
 
-		private void RemoveScene(Scene scene)
-		{
-			Debug.Assert(_scenes.Contains(scene));
-			_scenes.Remove(scene); 
-		}
 
-		public static Project Load(string file)
-		{
-			Debug.Assert(File.Exists(file));
-			return Serializer.FromFile<Project>(file);
-		}
+        private static string GetConfigurationName(BuildConfiguration config) => _buildConfigurationNames[(int)config];
 
-		public void Unload()
-		{
-			VisualStudio.CloseVisualStudio();
-			UndoRedo.Reset();
-		}
-
-		public static void Save(Project project)
-		{
-			Serializer.ToFile(project, project.FullPath);
-			Logger.Log(MessageType.Info, $"Project saved to {project.FullPath}");
-		}
-
-		[OnDeserialized]
-		private void OnDeserialized(StreamingContext context)
-		{
-			if (_scenes != null)
-			{
-				Scenes = new ReadOnlyObservableCollection<Scene>(_scenes);
-				OnPropertyChanged(nameof(Scenes));
-			}
-			ActiveScene = Scenes.FirstOrDefault(x => x.IsActive);
-
-			AddSceneCommand = new RelayCommand<Object>(x =>
-			{
-                AddScene($"New Scene {_scenes.Count}");
-				var newScene = _scenes.Last();
-				var sceneIndex = _scenes.Count - 1;
-				UndoRedo.Add(new UndoRedoAction(
-					() => RemoveScene(newScene),
-					() => _scenes.Insert(sceneIndex, newScene),
-					$"Add {newScene.Name}"));
-			});
-
-			RemoveSceneCommand = new RelayCommand<Scene>(x =>
-			{
-				var sceneIndex = _scenes.IndexOf(x);
-                RemoveScene(x);
-				UndoRedo.Add(new UndoRedoAction(
-					() => _scenes.Insert(sceneIndex, x),
-					() => RemoveScene(x),
-					$"Remove {x.Name}"));
-			}, x=> !x.IsActive);
-
-			UndoCommand = new RelayCommand<object>(x => UndoRedo.Undo());
-            RedoCommand = new RelayCommand<object>(x => UndoRedo.Redo());
-			SaveCommand = new RelayCommand<object>(x => Save(this));
+        private void AddScene(string sceneName)
+        {
+            Debug.Assert(!string.IsNullOrEmpty(sceneName.Trim()));
+            _scenes.Add(new Scene(this, sceneName));
         }
 
-		public Project(string name, string path)
-		{
-			Name = name;
-			Path = path;
+        private void RemoveScene(Scene scene)
+        {
+            Debug.Assert(_scenes.Contains(scene));
+            _scenes.Remove(scene);
+        }
 
-			OnDeserialized(new StreamingContext());
-		}
-		
-	}
+        public static Project Load(string file)
+        {
+            Debug.Assert(File.Exists(file));
+            return Serializer.FromFile<Project>(file);
+        }
+
+        public void Unload()
+        {
+            VisualStudio.CloseVisualStudio();
+            UndoRedo.Reset();
+        }
+
+        public static void Save(Project project)
+        {
+            Serializer.ToFile(project, project.FullPath);
+            Logger.Log(MessageType.Info, $"Project saved to {project.FullPath}");
+        }
+
+        private void BuildGameCodeDll(bool showWindow = true)
+        {
+            try
+            {
+                UnloadGameCodeDll();
+                VisualStudio.BuildSolution(this, GetConfigurationName(DllBuildConfig), showWindow);
+                if (VisualStudio.BuildSucceeded)
+                {
+                    LoadGameCodeDll();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+
+        }
+         
+        private void LoadGameCodeDll()
+        {
+            var configName = GetConfigurationName(DllBuildConfig);
+            var dll = $@"{Path}x64\{configName}\{Name}.dll";
+            if(File.Exists(dll) && EngineAPI.LoadGameCodeDll(dll) !=0)
+            {
+                Logger.Log(MessageType.Info, "Game code DLL loaded successfully");
+            }
+            else
+            {
+                Logger.Log(MessageType.Warning, "Failed to load game code DLL file. Try to build the project first.");
+            }
+        }
+        private void UnloadGameCodeDll()
+        {
+            if(EngineAPI.UnloadGameCodeDll() != 0)
+            {
+                Logger.Log(MessageType.Info, "Game code DLL unloaded");
+            }
+        }
+
+
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            if (_scenes != null)
+            {
+                Scenes = new ReadOnlyObservableCollection<Scene>(_scenes);
+                OnPropertyChanged(nameof(Scenes));
+            }
+            ActiveScene = Scenes.FirstOrDefault(x => x.IsActive);
+
+            BuildGameCodeDll(false);
+
+            AddSceneCommand = new RelayCommand<Object>(x =>
+            {
+                AddScene($"New Scene {_scenes.Count}");
+                var newScene = _scenes.Last();
+                var sceneIndex = _scenes.Count - 1;
+                UndoRedo.Add(new UndoRedoAction(
+                    () => RemoveScene(newScene),
+                    () => _scenes.Insert(sceneIndex, newScene),
+                    $"Add {newScene.Name}"));
+            });
+
+            RemoveSceneCommand = new RelayCommand<Scene>(x =>
+            {
+                var sceneIndex = _scenes.IndexOf(x);
+                RemoveScene(x);
+                UndoRedo.Add(new UndoRedoAction(
+                    () => _scenes.Insert(sceneIndex, x),
+                    () => RemoveScene(x),
+                    $"Remove {x.Name}"));
+            }, x => !x.IsActive);
+
+            UndoCommand = new RelayCommand<object>(x => UndoRedo.Undo(), x => UndoRedo.UndoList.Any());
+            RedoCommand = new RelayCommand<object>(x => UndoRedo.Redo(), x => UndoRedo.UndoList.Any());
+            SaveCommand = new RelayCommand<object>(x => Save(this));
+            BuildCommand = new RelayCommand<bool>(x => BuildGameCodeDll(x), x => !VisualStudio.IsDebugging() && VisualStudio.BuildDone);
+        }
+
+
+        public Project(string name, string path)
+        {
+            Name = name;
+            Path = path;
+
+            OnDeserialized(new StreamingContext());
+        }
+
+    }
 }
