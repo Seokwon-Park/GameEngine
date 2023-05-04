f14a384dafc61e64e46dec88d057144421bfe8db
rty33 <rty3394@naver.com>
2023-05-04 18:37:17 +0900

    Reading data into the geometry asset class

--- PrimalEditor/Common/Helpers.cs
+++ PrimalEditor/Common/Helpers.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 
-namespace PrimalEditor.Components
+namespace PrimalEditor
 {
     static class VisualExtensions
     {
@@ -15,15 +16,31 @@ namespace PrimalEditor.Components
             if (!(depObject is Visual)) return null;
 
             var parent = VisualTreeHelper.GetParent(depObject);
-            while(parent != null)
+            while (parent != null)
             {
-                if(parent is T type)
+                if (parent is T type)
                 {
                     return type;
                 }
                 parent = VisualTreeHelper.GetParent(parent);
             }
             return null;
+        }
+    }
+   
+    public static class ContentHelper
+    {
+        public static string GetRandomString(int length = 8)
+        {
+            if (length <= 0) length = 8;
+            var n = length / 11;
+            var sb = new StringBuilder();
+            for(int i = 0; i<=n; ++i)
+            {
+                sb.Append(Path.GetRandomFileName().Replace(".", ""));
+            }
+ 
+            return sb.ToString(0, length);
         }
     }
 }
--- PrimalEditor/Utilities/RenderSurface/RenderSurfaceView.xaml.cs
+++ PrimalEditor/Utilities/RenderSurface/RenderSurfaceView.xaml.cs
@@ -1,4 +1,4 @@
-﻿using PrimalEditor.Components;
+﻿using PrimalEditor;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
