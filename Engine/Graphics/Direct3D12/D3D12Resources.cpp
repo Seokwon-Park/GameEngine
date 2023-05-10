16b62108a398eb690530d084e9c3d43aa41c13e8
rty33 <rty3394@naver.com>
2023-05-10 19:21:11 +0900

    Finishing descriptor heap allocator

--- Engine/Graphics/Direct3D12/D3D12Resources.cpp
+++ Engine/Graphics/Direct3D12/D3D12Resources.cpp
@@ -38,6 +38,7 @@ namespace primal::graphics::d3d12
 		_size = 0;
 
 		for (u32 i{ 0 }; i < capacity; ++i) _free_handles[i] = i;
+		DEBUG_OP(for (u32 i{ 0 }; i < frame_buffer_count; ++i) assert(_deferred_free_indices[i].empty()));
 
 		_descriptor_size = device->GetDescriptorHandleIncrementSize(_type);
 		_cpu_start = _heap->GetCPUDescriptorHandleForHeapStart();
@@ -49,7 +50,26 @@ namespace primal::graphics::d3d12
 
 	void descriptor_heap::release()
 	{
+		assert(!_size);
+		core::deferred_release(_heap);
+	}
 
+	void descriptor_heap::process_deferred_free(u32 frame_idx)
+	{
+		std::lock_guard lock{ _mutex };
+		assert(frame_idx < frame_buffer_count);
+
+		utl::vector<u32>& indices{ _deferred_free_indices[frame_idx] };
+		if (!indices.empty())
+		{
+			for (auto index : indices)
+			{
+				--_size;
+				_free_handles[_size] = index;
+			}
+			indices.clear();
+
+		}
 	}
 
 	descriptor_handle descriptor_heap::allocate()
@@ -86,7 +106,9 @@ namespace primal::graphics::d3d12
 		const u32 index{ (u32)(handle.cpu.ptr - _cpu_start.ptr) / _descriptor_size };
 		assert(handle.index == index);
 
-
-		handle = {  };
+		const u32 frame_index{ core::current_frame_index() };
+		_deferred_free_indices[frame_index].push_back(index);
+		core::set_deferred_releases_flag();
+		handle = {};
 	}
 }
